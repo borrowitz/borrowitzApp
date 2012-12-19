929a8944b9f05844820721db00e5054c8f8d8c75
Dennis Zelada <denniszelada@gmail.com>
2012-12-19 10:36:32 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borrowitzapp::Application.config.secret_token = 'bf7b654b52dd71b709bdfd82eaa389dfd23c8ee5f5f1ccc7b5d7ade99dc5b9fc5403f763d71b03f65969085ad0af4d2fb293014d66fae2f6f22546d19d1b510b'
